6999ad98129ac19c3d663154066eb4e4d5183eca
devspsec <devs.psec@gmail.com>
2020-11-13 11:26:27 +0700

    add new component

--- Dashboard-demo/src/app/app.module.ts
+++ Dashboard-demo/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ReportComponent } from './components/report/report.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { InformationComponent } from './components/information/information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ReportComponent,
+    CalendarComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
